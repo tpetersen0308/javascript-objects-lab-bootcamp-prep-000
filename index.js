e0e354a0683494f00b79b78c4844484196e7889d
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-21 22:41:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object)
-  delete newObj[key]
-  return newObj
+  return delete Object.assign({}, object)[key]
+  
 }
